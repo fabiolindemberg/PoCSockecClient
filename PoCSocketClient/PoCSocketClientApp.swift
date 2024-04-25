af93943204eded380f5df8adc92217f72466a5ce
Fabio L. Peirera <fabiolindemberg@gmail.com>
2024-04-25 09:28:03 -0300

    Initial Commit

---
+++ PoCSocketClient/PoCSocketClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoCSocketClientApp.swift
+//  PoCSocketClient
+//
+//  Created by Fabio Pereira on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PoCSocketClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
